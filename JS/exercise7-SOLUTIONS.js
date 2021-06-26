c06602b4b8194f3a06778b24461a4aed6069e6ff
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-06-26 21:38:11 +0530

    To Do List in JS

--- JS/exercise7-SOLUTIONS.js
+++ JS/exercise7-SOLUTIONS.js
@@ -1,11 +1,10 @@
 // Create an object and an array which we will use in our facebook exercise. 
 
 // 1. Create an object that has properties "username" and "password". Fill those values in with strings.
-{
+var obj = {
 	username: "andrei",
 	password: "supersecret"
-}
-
+};
 // 2. Create an array which contains the object you have made above and name the arry "database"
 var database = [
 	{
