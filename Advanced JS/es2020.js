0d038905b640f860ce414911e0a0ae868750865b
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-06-28 16:51:52 +0530

    ES 2020 done

--- Advanced JS/es2020.js
+++ Advanced JS/es2020.js
@@ -1,9 +1,11 @@
 // Exercise 1: what do you think the MIN_SAFE_INTEGER is?
-console.log(Number.MAX_SAFE_INTEGER)
-console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MAX_SAFE_INTEGER) //9007199254740991
+console.log(Number.MIN_SAFE_INTEGER) //-9007199254740991
 
 // Exercise 2: why does this throw an error? How can you fix it?
 3 + 4 + 1n
+3 + 4 + 1 //8
+3n + 4n + 1n //8n
 
 // Exercise 3: Clean up this code using optional chaining
 let will_pokemon = {
@@ -12,7 +14,7 @@ let will_pokemon = {
         height: 0.4,
         weight: 6,
         power: 'lightning',
-        friend: { 
+        friend: {
             charizard: {
                 species: 'Dragon Pokemon',
                 height: 1.7,
@@ -32,18 +34,16 @@ let andrei_pokemon = {
     }
 }
 
-if (andrei_pokemon && andrei_pokemon.raichu && will_pokemon 
-    && will_pokemon.pikachu && will_pokemon.pikachu.friend 
-    && will_pokemon.pikachu.friend.charizard) {
-        console.log('fight!')
-    } else {
-        console.log('walk away...')
-    }
+if (andrei_pokemon?.raichu && will_pokemon?.pikachu?.friend?.charizard) {
+    console.log('fight!')
+} else {
+    console.log('walk away...')
+}
 
 
 // Exercise 4: What do these each output?
-console.log(false ?? 'hellooo')
-console.log(null ?? 'hellooo')
-console.log(null || 'hellooo')
-console.log((false || null) ?? 'hellooo')
-console.log(null ?? (false || 'hellooo'))
+console.log(false ?? 'hellooo') //false
+console.log(null ?? 'hellooo') //'hellooo'
+console.log(null || 'hellooo') //'hellooo'
+console.log((false || null) ?? 'hellooo') //'hellooo'
+console.log(null ?? (false || 'hellooo')) //'hellooo'
