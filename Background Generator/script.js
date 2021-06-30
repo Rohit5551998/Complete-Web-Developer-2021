4297125959d6c528123b41cf1b8fae413a0e7c08
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-06-30 18:48:37 +0530

    Revert "Updates"
    This reverts commit 35b8954ff4d6de87c6929ead5da61fee9ad7649f.
    
    Restore

---
+++ Background Generator/script.js
@@ -0,0 +1,55 @@
+var _ = require('lodash');
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log("answer:", _.without(array, 3));
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var bgColor = window.getComputedStyle(body).backgroundImage;
+var button = document.querySelector("button");
+
+//Initial colors and h3
+function componentToHex(c) {
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+var colorArray = bgColor;
+var rgb = [];
+colorArray = colorArray.split(", ");
+colorArray.forEach((element) => { rgb.push(element.replace(/[^0-9]/g, "")); });
+rgb.splice(0, 1);
+color1.value = rgbToHex(Number(rgb[0]), Number(rgb[1]), Number(rgb[2]));
+color2.value = rgbToHex(Number(rgb[3]), Number(rgb[4]), Number(rgb[5]));
+setGradient();
+
+//Existing code
+function setGradient() {
+	body.style.background =
+		"linear-gradient(to right, "
+		+ color1.value
+		+ ", "
+		+ color2.value
+		+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+//Random Colors
+function randomcolors() {
+	color1.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+	setGradient();
+}
+
+button.addEventListener("click", randomcolors);
---
+++ README.md
@@ -0,0 +1,10 @@
+# Complete-Web-Developer-2021
+This repo covers ZTM's Complete Web Developer 2021 course contents and also several projects that I implemented during this entire span
+
+
+[Startup of the Year](https://rohit5551998.github.io/Complete-Web-Developer-2021/Startup%20of%20the%20Year%20Project/) Mini-Project hosted here.
+
+[Prettify Layout](https://rohit5551998.github.io/Complete-Web-Developer-2021/CSS%20Layout/) Mini-Project hosted here.
+
+[Random Gradient Background Generator](https://rohit5551998.github.io/Complete-Web-Developer-2021/Background%20Generator/) Mini-Project hosted here.
+
