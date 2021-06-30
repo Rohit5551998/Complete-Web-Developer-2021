985d12c5c88871aaf0a1bddd3f4c22e27264e998
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-06-30 12:28:32 +0530

    Building React App 1-3

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import SearchBox from './SearchBox';
+import CardList from './CardList';
+import { robots } from './robots';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        console.log(filteredRobots);
+        return (
+            <div className="tc" >
+                <h1>Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
 }
 
 export default App;
