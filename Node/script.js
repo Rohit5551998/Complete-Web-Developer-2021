89d0e3fdcfc845617afedcf7dc92d7a7b9757b75
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-08-01 20:20:15 +0530

    ES6 + import/export

---
+++ Node/script.js
@@ -0,0 +1,11 @@
+// React(ES6) way 
+// import largeNumber from './script2.js';
+
+// const script2 = require("./script2");
+
+import { largeNumber } from "./script2.js";
+
+const a = largeNumber;
+const b = 5;
+
+console.log(a, b);
---
+++ Node/script2.js
@@ -0,0 +1,7 @@
+export const largeNumber = 356;
+
+//React way(ES6) export default largeNumber;
+
+// module.exports = {
+//     largeNumber: largeNumber
+// };
