dfbcf744ec04c7cf738c731f875dfd5d9cf17c3a
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-08-02 12:40:59 +0530

    Express Intro

--- Node/server.js
+++ Node/server.js
@@ -1,15 +1,43 @@
-const http = require('http');
+// Without Express
 
-const server = http.createServer((request, response) => {
-    // console.log(request.headers)
-    console.log(request.method)
-    console.log(request.url)
-    const user = {
-        name: 'John',
-        hobby: 'Skating'
-    }
-    response.setHeader('Content-Type', 'text/html');
-    response.end(JSON.stringify(user));
+// const http = require('http');
+
+// const server = http.createServer((request, response) => {
+//     // console.log(request.headers)
+//     console.log(request.method)
+//     console.log(request.url)
+//     const user = {
+//         name: 'John',
+//         hobby: 'Skating'
+//     }
+//     response.setHeader('Content-Type', 'text/html');
+//     response.end(JSON.stringify(user));
+// })
+
+// server.listen(3001);
+
+
+
+// With Express
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("getting root")
 })
 
-server.listen(3001);
+app.get('/profile', (req, res) => {
+    res.send("getting profile")
+})
+
+app.post('/profile', (req, res) => {
+    const user = {
+        name: 'Sally',
+        hobby: 'Soccer'
+    }
+    res.send(user);
+})
+
+app.listen(3001);
